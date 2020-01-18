617e391bd2ec7a7a34608cc10d76aea2d619333e
JasperDre <jasperdre@gmail.com>
2020-01-18 20:48:04 +0100

    Add initial Java and Spigot project

---
+++ TinyChatManager/src/gloryrock/tinychatmanager/TinyChatManager.java
@@ -0,0 +1,15 @@
+package gloryrock.tinychatmanager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TinyChatManager extends JavaPlugin {
+  //Fired when plugin is first enabled
+  @Override
+  public void onEnable() {
+  }
+  
+  // Fired when plugin is disabled
+  @Override
+  public void onDisable() {
+
+  }
+}
